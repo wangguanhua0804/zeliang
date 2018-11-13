10d001f9245fee10780be2a23ada81cae650f57b
wangguanhua <guanhuawang@jieyuechina.com>
2018-11-13 19:13:33 +0800

    pom文件引入 打包插件

--- src/main/java/com/wgh/springboot/Application.java
+++ src/main/java/com/wgh/springboot/Application.java
@@ -2,12 +2,20 @@ package com.wgh.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
 @ComponentScan(basePackages={"com.wgh.springboot.*"})
-public class Application {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
+
+   /* @Override//为了打包springboot项目
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }*/
 }
