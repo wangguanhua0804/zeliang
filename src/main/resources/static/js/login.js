4e16fa083411bd18da0ee2676197ab08239c69ba
wangguanhua <guanhuawang@jieyuechina.com>
2018-11-14 16:17:26 +0800

    系统用户信息展示

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -15,10 +15,25 @@ $("#loginForm").submit(function () {
         data:JSON.stringify(obj),
         success:function (result) {
             if(result.rspCode==="200"){
-                localStorage.setItem("userAll",result.body);
+                /*  //本来想用一个对象存放所有信息,存放失败
+                var sysUser={
+                    "id":result.body.id,
+                    "user":result.body.user,
+                    "userName":result.body.userName,
+                    "password":result.body.password,
+                    "idCard":result.body.idCard,
+                    "token":result.body.token,
+                    "mobile":result.body.mobile,
+                    "role":result.body.role.role_name,
+                }
+                localStorage.setItem("sysUser",sysUser);
+                alert(user.token);*/
                 localStorage.setItem("user",user);
                 localStorage.setItem("userId",result.body.id);
                 localStorage.setItem("token",result.body.token);
+                localStorage.setItem("mobile",result.body.mobile);
+                localStorage.setItem("idCard",result.body.idCard);
+                localStorage.setItem("role_name",result.body.role.role_name);
                 localStorage.setItem("userName",result.body.userName);
                 window.location.href ="home.html";
             }else {
