9f0e8c92456d441041b4c9b55bfa22408b16b614
geyy <18822677837@163.com>
2019-03-04 11:22:56 +0800

    添加单元测试

---
+++ src/test/MyTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import org.springframework.test.context.junit4.SpringRunner;
+
+public class MyTest {
+    @Test
+    public void run1(){
+        String a="1";
+        System.out.println(a);
+    }
+}
