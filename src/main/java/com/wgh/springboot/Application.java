b706b3ab062d125aa971f10b40adb2ef63e515f9
geyy <18822677837@163.com>
2019-03-09 11:26:36 +0800

    bug修复

--- src/main/java/com/wgh/springboot/Application.java
+++ src/main/java/com/wgh/springboot/Application.java
@@ -1,13 +1,15 @@
 package com.wgh.springboot;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-
-@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
+@MapperScan("com.wgh.springboot.mapper.*")
+@SpringBootApplication//(exclude = DataSourceAutoConfiguration.class)
 @ComponentScan(basePackages={"com.wgh.springboot.*"})
 public class Application /*extends SpringBootServletInitializer*/ {
     public static void main(String[] args) throws Exception {
